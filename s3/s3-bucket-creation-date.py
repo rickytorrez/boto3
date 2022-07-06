6c756ea18b56fe8f7ab635542d36530476e2073e
EC2 Default User <ec2-user@ip-172-31-91-208.ec2.internal>
2022-07-06 14:57:03 +0000

    bucket creation date

---
+++ s3/s3-bucket-creation-date.py
@@ -0,0 +1,10 @@
+import boto3
+
+s3 = boto3.client('s3')
+
+response = s3.list_buckets()['Buckets']
+
+# buckets = response['Buckets']
+
+for bucket in response:
+    print(bucket['Name'], '-', bucket['CreationDate'])
